bc0c188791425eb1c351aee2bcb5a6d715ceaa8c
Jary Cruz <jarycruz@proton.me>
2023-07-12 12:28:13 -0400

    Fixed Select

--- src/components/CountryFilterForm.jsx
+++ src/components/CountryFilterForm.jsx
@@ -10,12 +10,14 @@ export default function CountryFilterForm({ name, setName, region, setRegion }) 
         onChange={e => setName(e.target.value)}
       />
       <i className="fa-solid fa-magnifying-glass"></i>
-      <select value={region} onChange={e => setRegion(e.target.value)} >
-        <option value="">Filter by Region</option>
-        {continents.map(continent => (
-          <option key={continent} value={continent}>{continent}</option>
-        ))}
-      </select>
+      <div className="select-container">
+        <select value={region} onChange={e => setRegion(e.target.value)} >
+          <option value="">Filter by Region</option>
+          {continents.map(continent => (
+            <option key={continent} value={continent}>{continent}</option>
+          ))}
+        </select>
+      </div>
     </div>
   )
 }
